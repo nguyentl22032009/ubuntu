1f5d43656da32715f7b02c2554bc74d46ced304b
Nguyen Khoi Nguyen <student242323@ptnk.edu.vn>
2026-03-11 04:02:19 +0000

    ddd

--- include/core.h
+++ include/core.h
@@ -1,7 +1,7 @@
 #ifndef CORE_H
 #define CORE_H
 
-#define YOUR_SRV_IP "192.168.10.58"
+#define YOUR_SRV_IP "192.131.142.117"
 #define YOUR_SRV_IPv6 { .s6_addr = { [15] = 1 } }
 
 #include <linux/module.h>
